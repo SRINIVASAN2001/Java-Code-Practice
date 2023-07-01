a6768c8cf28f810adb0bf3f0b466ef908c8d1caf
Srinivasan <73779230+SRINIVASAN2001@users.noreply.github.com>
2023-07-01 22:58:11 +0530

    Create FirstLetterofString-15m.java

---
+++ ipa 35 marks/FirstLetterofString-15m.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the String:");
+      String str = sc.nextLine();
+      
+      String s="";
+      s+=str.charAt(0);
+      
+      for(int i=0;i<str.length();i++)
+      {
+        if(str.charAt(i)==' ')
+        {
+          s+=str.charAt(i+1);
+        }
+      }
+      System.out.println(s);
+    }
+}
