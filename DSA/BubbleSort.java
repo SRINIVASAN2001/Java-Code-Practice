ce34f09f051aad2a3377e99b1e2050a75e85f07e
Srinivasan <73779230+SRINIVASAN2001@users.noreply.github.com>
2023-07-06 22:56:16 +0530

    Create BubbleSort.java

---
+++ DSA/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class solution
+{
+  public static void main (String[] args)
+  {
+    int[] arr ={2,5,4,1,6};
+    
+    BubbleSort(arr);
+    
+    for(int i:arr)
+    {
+      System.out.print(i+" ");
+    }
+  }
+  
+  public static void BubbleSort(int[] arr)
+  {
+    int temp=0;
+    
+    for(int i=0;i<arr.length;i++)
+    {
+      for(int j=i+1;j<arr.length;j++)
+      {
+        if(arr[i]>arr[j])
+        {
+          temp=arr[i];
+          arr[i]=arr[j];
+          arr[j]=temp;
+        }
+      }
+    }
+  }
+}
